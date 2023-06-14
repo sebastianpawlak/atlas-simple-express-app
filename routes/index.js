02781bf7d0821380e2153a9a355bb825df200300
Sebastian Pawlak <sebastian.pawlak@wpengine.com>
2023-06-14 17:04:26 +0200

    my third commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post("/upload", (req, res, next) => {
+  const form = formidable({
+    uploadDir: "/var/tmp",
+    keepExtensions: false,
+    maxFileSize: 200 * 1024 * 1024
+  });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.status(500).json({ err });
+      return;
+    }
+    res.status(200).json({ fields, files });
+  });
+});
+
 module.exports = router;
