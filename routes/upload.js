b12e3ab20a47410a53faad92499ba3b5d0b1545e
Sebastian Pawlak <sebastian.pawlak@wpengine.com>
2023-06-16 14:23:36 +0200

    set the same filename

--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const formidable = require("formidable");
+var fs = require('fs');
 
 router.post("/image", (req, res) => {
   const form = formidable({
     uploadDir: "/var/tmp",
     keepExtensions: false,
-    maxFileSize: 200 * 1024 * 1024
+    maxFileSize: 200 * 1024 * 1024,
+    filename: (name, ext, part, form) => {
+      return "the_same"; // Will be joined with options.uploadDir.
+    }
   });
 
   form.parse(req, (err, fields, files) => {
